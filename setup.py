1eb75c5731b29c2438c8adf37a3fa59211a0df7f
cleo <cleo24620@gmail.com>
2024-10-13 10:57:40 +0800

    the raw version, waiting for update.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='crawler',
+    version='0.1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='cleo',
+    author_email='cleo24620@gmail.com',
+    description='download files of a webpage',
+    operating_system='windows 11',
+    python_requires='3.12.7'
+)
